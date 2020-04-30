6fc3847864dfd33433b4e9e924c694ac7c68c1fa
Felipe Thadeu <felipet65@gmail.com>
2020-04-29 21:29:46 -0300

    code created

---
+++ app.py
@@ -0,0 +1,45 @@
+# encoding: utf-8
+# encoding: iso-8859-1
+# encoding: win-1252
+import json
+import requests
+import string
+import hashlib
+
+api_url_base = 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=006302976f90a5a0dc663f235e93a5e43bb73182'
+alfabeto = string.ascii_lowercase
+resultado = ''
+response = requests.get(api_url_base)
+response_json = response.json()
+
+numero_casas = response_json['numero_casas']
+response_array = list(response_json['cifrado'].lower())
+
+def fraseCifrada(arquivo):                      #Função que cria o arquivo Json
+    with open('answer.json', 'w') as write:
+        json.dump(arquivo, write)
+
+fraseCifrada(response_json)
+
+for letra in response_array:
+    if letra in string.whitespace: #Verifica se há espaços em branco e inclui sem modificação
+        resultado = resultado + letra
+    elif letra in string.digits:   #Verifica se há dígitos e inclui sem modificação
+        resultado = resultado + letra
+    elif letra in string.punctuation: #Verifica se há pontuação e inclui sem modificação
+        resultado = resultado + letra
+    elif letra in alfabeto:             #faz a decriptografia
+        posicao = alfabeto.find(letra)
+        posicao = (posicao - numero_casas) % 26
+        resultado = resultado + alfabeto[posicao]
+
+response_json['decifrado'] = resultado
+response_json['resumo_criptografico'] = hashlib.sha1(resultado.encode('utf-8')).hexdigest()
+
+def fraseDecifrada(arquivo):                      #Função que cria o arquivo Json
+    with open('answer.json', 'w') as write:
+        json.dump(arquivo, write)
+
+fraseDecifrada(response_json)
+
+r = requests.post('https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=006302976f90a5a0dc663f235e93a5e43bb73182', files={"answer": open("answer.json", "rb")})
